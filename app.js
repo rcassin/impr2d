b0b68662cbf8baaf4532c4186b0a038c346931b5
Louis Desplanche <Louis Desplanche>
2017-05-05 22:46:04 +0200

    init

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+var five = require("johnny-five");
+var Raspi = require("raspi-io");
+var board = new five.Board({
+  io: new Raspi()
+});
+
+board.on("ready", function() {
+  console.log(":)");
+});
+
+
 console.log("Start");
